a56119953ed8c318d402cb5a5afd7f12f69e2945
vielinko <victor.lutin@gmail.com>
2020-07-02 12:38:26 -0600

    heritance practice

--- aws_ml_excersices/gaussian/gaussian.py
+++ aws_ml_excersices/gaussian/gaussian.py
@@ -28,7 +28,7 @@ class Gaussian(Distribution):
             float: mean of the data set
         """
 
-        self.mean = sum(self.data) / len(self.data)
+        self.mean = 1.0 * sum(self.data) / len(self.data)
         return self.mean
 
 
@@ -51,32 +51,24 @@ class Gaussian(Distribution):
         self.stdev = math.sqrt(accumulate / divideBy)
         return self.stdev
 
-    # def read_data_file(self, file_name, sample=True):
+    def read_data_file(self, file_name, sample=True):
 
-    #     """Method to read in data from a txt file. The txt file should have
-    #     one number (float) per line. The numbers are stored in the data attribute.
-    #     After reading in the file, the mean and standard deviation are calculated
+        """Method to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute.
+        After reading in the file, the mean and standard deviation are calculated
 
-    #     Args:
-    #         file_name (string): name of a file to read from
+        Args:
+            file_name (string): name of a file to read from
 
-    #     Returns:
-    #         None
+        Returns:
+            None
 
-    #     """
+        """
 
-    #     # This code opens a data file and appends the data to a list called data_list
-    #     with open(file_name) as file:
-    #         data_list = []
-    #         line = file.readline()
-    #         while line:
-    #             data_list.append(int(line))
-    #             line = file.readline()
-    #     file.close()
+        Distribution.read_data_file(self, file_name)
 
-    #     self.data = data_list
-    #     self.calculate_mean()
-    #     self.calculate_stdev(sample)
+        self.calculate_mean()
+        self.calculate_stdev(sample)
 
     def plot_histogram(self):
         """Method to output a histogram of the instance variable data using
